9fa0d30688eab4c42a6b832638cff4de0ebc8887
tjards <travis.jardine@gmail.com>
2023-06-28 08:17:36 -0400

    hamilton product working

--- main.py
+++ main.py
@@ -6,34 +6,38 @@ Created on Wed Jun 28 07:35:05 2023
 @author: tjards
 """
 
+# import stuff
 from sympy import *
+
+
+# Hamilton product
+# ----------------
+def hamilton_product(q1, q2):
+    w1, x1, y1, z1 = q1
+    w2, x2, y2, z2 = q2
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
+    z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
+    return Matrix([w, x, y, z])
+
 
 # define quatertions we want to manipulate
 # ---------------------------------------
+# define our variables
+var('w x y z')
+q1 = Matrix([w, x, y, z])
 
-#q1 = np.array([w, x, y, z])
+var('a b c d')
+q2 = Matrix([a, b, c, d])
 
-
-
-
-
-a       = Symbol('x')
-x,y,z   = symbols('p,q,r')
-
-
-print(a + a + 1)
-print(x)
+product = hamilton_product(q1, q2)
 
 
+pprint(product) # pprint is "pretty print"
 
 
-# multiplication between quaternions
-# -----------------------------------
-# def quat_mult(q1, q2):
-#     w1, x1, y1, z1 = q1
-#     w2, x2, y2, z2 = q2
-#     w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
-#     x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
-#     y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
-#     z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
-#     return np.array([w, x, y, z])
+    
+
+
+
