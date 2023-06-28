f71c767b90a37acfd65c5b68458ed7c083c6d401
tjards <travis.jardine@gmail.com>
2023-06-28 08:30:36 -0400

    conjugate working

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ Created on Wed Jun 28 07:35:05 2023
 # import stuff
 from sympy import *
 
-
 # Hamilton product
 # ----------------
 def hamilton_product(q1, q2):
@@ -21,20 +20,60 @@ def hamilton_product(q1, q2):
     z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
     return Matrix([w, x, y, z])
 
+# quaternion conjugate
+# --------------------
+def quatjugate(q):
+    w, x, y, z = q
+    return Matrix([w, -x, -y, -z])
+
 
 # define quatertions we want to manipulate
 # ---------------------------------------
-# define our variables
-var('w x y z')
-q1 = Matrix([w, x, y, z])
 
-var('a b c d')
-q2 = Matrix([a, b, c, d])
+# parametric equations for hypotrochoidal curves
+var('d r R t psi')
+psi = (R-r)/r
+w = 0
+x = (R-r)*cos(t)+d*cos(psi*t)
+y = (R-r)*sin(t)-d*sin(psi*t)
+z = 0
+eqn_hyp = Matrix([w,x,y,z])
+pprint(eqn_hyp)
 
-product = hamilton_product(q1, q2)
+# equation of circle with radius d
+# --------------------------------
+var('t')
+w = 0
+x = d*cos(t)
+y = d*sin(t)
+z = 0
+eqn_cir = Matrix([w,x,y,z])
+pprint(eqn_cir)
+
+# unknowns
+# --------
+var('a b')
+u1 = Matrix([a,b,0,0])
+u2 = quatjugate(u1)
+pprint(u1)
+pprint(u2)
 
 
-pprint(product) # pprint is "pretty print"
+
+
+
+
+
+
+
+# examples
+# --------
+# var('w x y z')
+# q1 = Matrix([w, x, y, z])
+# var('a b c d')
+# q2 = Matrix([a, b, c, d])
+# product = hamilton_product(q1, q2)
+# pprint(product)
 
 
     
