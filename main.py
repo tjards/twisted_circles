a32985670ad0424795e9c1f2ed64be80cf1c9938
tjards <travis.jardine@gmail.com>
2023-07-02 11:57:04 -0400

    playing with curves

--- main.py
+++ main.py
@@ -4,6 +4,12 @@
 Created on Wed Jun 28 07:35:05 2023
 
 @author: tjards
+
+play here:
+    
+    https://www.desmos.com/calculator/65ps3jwzqn
+
+
 """
 
 # import stuff
@@ -11,6 +17,7 @@ Created on Wed Jun 28 07:35:05 2023
 from sympy import *
 import matplotlib.pyplot as plt
 import numpy as np
+#import time
 
 
 # Hamilton product
@@ -52,6 +59,7 @@ def quatrotate(q, p):
 var('a b d r R t psi t z', positive = True)
 
 # hypotrochoidal curves
+# ----------------------
 psi = (R-r)/r
 w = 0
 x = (R-r)*cos(t)+d*cos(psi*t)
@@ -61,6 +69,7 @@ eqn_hyp = Matrix([w,x,y,z])
 #pprint(eqn_hyp)
 
 # lemniscate of Gerono
+# -------------------
 w = 0
 x = d*cos(t)
 y = d*sin(t)*cos(t)
@@ -69,6 +78,7 @@ eqn_8_1 = Matrix([w,x,y,z])
 #pprint(eqn_8_1)
 
 # lemniscate of Bernoulli
+# -----------------------
 c2 = d   # half-width: distance from crossing point to horiz extreme (tunable)
 w = 0
 x = c2*cos(t)/(1+sin(t)*sin(t))
@@ -76,6 +86,36 @@ y = c2*sin(t)*cos(t)/(1+sin(t)*sin(t))
 z = 0 #0.5*d*sin(t)*sin(t)
 eqn_8_2 = Matrix([w,x,y,z])
 #pprint(eqn_8_2)
+
+# Vivians curve
+# https://mathworld.wolfram.com/VivianisCurve.html
+# -------------
+c3 = d
+w = 0
+x = c3*(1+cos(t))
+y = c3*sin(t)
+z = 2*c3*sin(t/2)
+eqn_8_3 = Matrix([w,x,y,z])
+
+# dumbell curve (sextic curve), bowtie
+# https://mathcurve.com/courbes2d.gb/doublegouttedeau/doublegouttedeau.shtml
+# -----------------------------------
+c4 = d
+c5 = d
+w = 0
+x = c4*cos(t)
+y = ((c4*c4)/c5)*cos(t)*cos(t)*sin(t)
+z = 0
+eqn_8_4 = Matrix([w,x,y,z])
+
+#Cayley's sextet
+# --------------
+w = 0
+x = cos(t)*cos(t)*cos(t)*cos(3*t)
+y = cos(t)*cos(t)*cos(t)*sin(3*t)
+z = 0
+eqn_8_5 = Matrix([w,x,y,z])
+
 
 # # lemniscate of Booth (for later exploration)
 
@@ -124,7 +164,7 @@ Let us solve for a, b that generate a hypotrochoidal curves as
 '''
 #%%
 RHS = quatrotate(u,eqn_cir)
-LHS = eqn_8_2
+LHS = eqn_8_5
 #pprint(LHS)
 #print('=')
 #pprint(RHS)
@@ -143,45 +183,69 @@ pprint(solutions.args[0].simplify())
 
 #%% plot stuff
 
-plt.figure()
-plt.axis([-2,2,-2,2])
-r1   = 1
-#plt_type = 'circle'
-#plt_type = 'gerono'
-plt_type = 'bernoulli'
-#plt_type = 'booth'
+# plt.figure()
+# plt.axis([-5,5,-5,5])
+# r1   = 1
+# #plt_type = 'circle'
+# #plt_type = 'gerono'
+# #plt_type = 'bernoulli'
+# plt_type = 'booth'
 
-if plt_type == 'booth':
+# if plt_type == 'booth':
     
-    # for oval: r/sqrt(2) < r2 < r*sqrt(2)
-    #r2  = r*sqrt(2)
-    r2  = r1/sqrt(2)
+#     # for oval: r/sqrt(2) < r2 < r*sqrt(2)
+#     #r2  = r*sqrt(2)
+#     r2  = r1/np.sqrt(2)
 
-    # for circle: r2 = r1
-    r2  = r1
+#     # for circle: r2 = r1
+#     r2  = r1
 
 
+# xs = 0
+# ys = 0
 
-for t in np.arange(0,2*np.pi,0.1):
+# #for rs in np.arange(-2*r1/np.sqrt(2),r1*np.sqrt(2)+2*r1/np.sqrt(2),0.25*r1/np.sqrt(2)):
+# #for rs in np.arange(-2*r1^2,2*r1^3,0.25*r1/np.sqrt(2)):
+# #for rs in np.arange(r1*sqrt(2),r1/sqrt(2)-r1/sqrt(2),-0.2):
+# for rs in [0,0.1,0.2,0.5,1,1.5,2]:
     
-    if plt_type == 'circle':
-        xs = r1*cos(t)
-        ys = r1*sin(t)
+#     r2 = 1
+#     r1 = rs
     
-    if plt_type == 'gerono':
-        xs = r1*cos(t)
-        ys = r1*sin(t)*cos(t)
-    
-    if plt_type == 'bernoulli':
-        xs = r1*cos(t)/(1+sin(t)*sin(t))
-        ys = r1*sin(t)*cos(t)/(1+sin(t)*sin(t))
+#     #if r2 >= - 0.2 and r2 <= 0.2:
         
-    if plt_type == 'booth':
-        xs = r2*r1*r2*cos(t)/(r2*r2*cos(t)*cos(t) + r1*r1*sin(t)*sin(t))
-        ys = r2*r1*r2*sin(t)/(r2*r2*cos(t)*cos(t) + r1*r1*sin(t)*sin(t))
-    
-    plt.plot(xs,ys,'-bo')
-plt.show()
+#         #continue
+
+#     for t in np.arange(0,2*np.pi,0.1):
+        
+#         xs_prev = xs
+#         ys_prev = ys
+        
+#         if plt_type == 'circle':
+#             xs = r1*cos(t)
+#             ys = r1*sin(t)
+        
+#         if plt_type == 'gerono':
+#             xs = r1*cos(t)
+#             ys = r1*sin(t)*cos(t)
+        
+#         if plt_type == 'bernoulli':
+#             xs = r1*cos(t)/(1+sin(t)*sin(t))
+#             ys = r1*sin(t)*cos(t)/(1+sin(t)*sin(t))
+            
+#         if plt_type == 'booth':
+#             xs = r2*r1*r2*cos(t)/(r2*r2*cos(t)*cos(t) + r1*r1*sin(t)*sin(t))
+#             ys = r2*r1*r2*sin(t)/(r2*r2*cos(t)*cos(t) + r1*r1*sin(t)*sin(t))
+        
+#         plt.axis([-5,5,-5,5])
+#         plt.text(-1.5,-1.2,'r1: '+str(r1))
+#         plt.text(-1.5,-1.5,'r2: '+str(r2))
+#         plt.pause(0.01)
+#         plt.plot([xs_prev,xs],[ys_prev, ys],'-bo')
+
+        
+#     plt.clf()
+# plt.show()
 
 
 
